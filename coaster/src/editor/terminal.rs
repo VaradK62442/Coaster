7b9670c2180935674eb056a0bc622c2f80be27f6
VaradK62442 <varadkulk123@gmail.com>
2025-05-19 22:03:45 +0100

    command trait to simplify queueing logic

--- coaster/src/editor/terminal.rs
+++ coaster/src/editor/terminal.rs
@@ -3,7 +3,7 @@ use crossterm::cursor::{
     Hide,
     Show
 };
-use crossterm::queue;
+use crossterm::{queue, Command};
 use crossterm::style::Print;
 use crossterm::terminal::{
     disable_raw_mode,
@@ -13,6 +13,7 @@ use crossterm::terminal::{
     ClearType
 };
 use std::io::{stdout, Error, Write};
+use core::fmt::Display;
 
 
 #[derive(Copy, Clone)]
@@ -47,32 +48,32 @@ impl Terminal {
     }
 
     pub fn clear_screen() -> Result<(), Error> {
-        queue!(stdout(), Clear(ClearType::All))?;
+        Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
 
     pub fn clear_line() -> Result<(), Error> {
-        queue!(stdout(), Clear(ClearType::CurrentLine))?;
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
     pub fn move_cursor_to(position: Position) -> Result<(), Error> {
-        queue!(stdout(), MoveTo(position.x, position.y))?;
+        Self::queue_command(MoveTo(position.x, position.y))?;
         Ok(())
     }
 
     pub fn hide_cursor() -> Result<(), Error> {
-        queue!(stdout(), Hide)?;
+        Self::queue_command(Hide)?;
         Ok(())
     }
 
     pub fn show_cursor() -> Result<(), Error> {
-        queue!(stdout(), Show)?;
+        Self::queue_command(Show)?;
         Ok(())
     }
 
-    pub fn print(string: &str) -> Result<(), Error> {
-        queue!(stdout(), Print(string))?;
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
         Ok(())
     }
 
@@ -83,6 +84,11 @@ impl Terminal {
 
     pub fn execute() -> Result<(), Error> {
         stdout().flush()?;
+        Ok(())
+    }
+
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
         Ok(())
     }
 }
