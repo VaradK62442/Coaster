073e2ee65263e476d23d9481af7ca17e84bce45d
VaradK62442 <varadkulk123@gmail.com>
2025-05-22 20:49:31 +0100

    add buffer and print all lines in buffer

--- coaster/src/editor/view.rs
+++ coaster/src/editor/view.rs
@@ -1,33 +1,41 @@
+mod buffer;
+
 use std::io::Error;
 use super::terminal::{Terminal, Size, Position};
+use buffer::Buffer;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-pub struct View;
+#[derive(Default)]
+pub struct View {
+    pub buffer: Buffer
+}
 
 impl View {
-    pub fn render() -> Result<(), Error> {
+    pub fn render(&self) -> Result<(), Error> {
         let Size {height, ..} = Terminal::size()?;
         for current_row in 0..height {
             Terminal::clear_line()?;
-            Terminal::print("~")?;
-            if current_row == 0 {
-                Terminal::print("Hello, World!")?;
+            if let Some(line) = self.buffer.lines.get(current_row) {
+                Terminal::print(line)?;
+                Terminal::print("\r\n")?;
+                continue;
             }
+            Terminal::print("~")?;
             if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
         }
 
-        View::draw_welcome_msg()?;
+        self.draw_welcome_msg()?;
 
         Ok(())
     }
     
     /// Prints the name and version of the editor in the middle of the terminal
     /// by moving the cursor and printing NAME and VERSION, as above.
-    fn draw_welcome_msg() -> Result<(), Error> {
+    fn draw_welcome_msg(&self) -> Result<(), Error> {
         let Size {height, width} = Terminal::size()?;
         let to_print = format!(">>> {NAME} - v{VERSION}");
         let third_height = height.saturating_div(4);
